4ff4c4aa29a7e61dcb5e08575e358c608d92ee30
Jason Ansel <jansel@fb.com>
2020-05-23 21:42:44 -0700

    wip

--- test/jit/paritybench.py
+++ test/jit/paritybench.py
@@ -4,9 +4,12 @@ import inspect
 import json
 import logging
 import os
+import random
 import re
+import tempfile
 import time
 import types
+import unittest
 import zipfile
 from collections import Counter, defaultdict
 from typing import List
@@ -21,19 +24,23 @@ log = logging.getLogger(__name__)
 
 NN_MODULE_RE = re.compile(r"(torch\.)?(nn\.Module|autograd\.Function)\b", re.MULTILINE)
 IMPORT_WHITELIST = ("torch",
-                    # TODO: "torchvision",
+                    # TODO: "torchvision",  is used by many
                     "math", "collections")
 
 
 class Stats(Counter):
+    """
+    Counter used to report totals by module
+    """
+
     def __str__(self):
-        stats_keys = (
+        stats_keys = [
             "total",
             "init_ok",
             "deduced_args_ok",
-            "jit_compiles",
             "jit_success",
-        )
+        ]
+        stats_keys = stats_keys + list(set(self.keys()) - set(stats_keys))
         return str([(k, self[k]) for k in stats_keys])
 
     def log(self, prefix: str):
@@ -41,13 +48,18 @@ class Stats(Counter):
 
 
 class ErrorAggregator(object):
+    """
+    Collect and group error messages for report at the end
+    """
+
     def __init__(self):
         super(ErrorAggregator, self).__init__()
         self.error_groups = []
         self.bigram_to_group_ids = defaultdict(list)
 
     def record(self, e: Exception):
-        error_msg = f"{e.__class__.__name__}: {e}"
+        ex_msg = str(e).strip().split('\n')[0]
+        error_msg = f"{e.__class__.__name__}: {ex_msg}"
         msg_words = list(re.findall(r"[a-zA-Z]+", error_msg))
         msg_bigrams = [f"{a}_{b}" for a, b in zip(msg_words, msg_words[1:])] or msg_words
 
@@ -69,12 +81,20 @@ class ErrorAggregator(object):
 
     def __str__(self):
         errors = sorted(self.error_groups, key=len, reverse=True)
-        return '\n'.join(f" - {len(e)} errors like: {e[0]}"
-                         for e in  errors[:10])
+        return '\n'.join(f"  - {len(e)} errors like: {e[0]}"
+                         for e in errors[:10])
 
-    @classmethod
-    def grouped(cls):
-        return defaultdict(cls)
+    def __len__(self):
+        return sum(map(len, self.error_groups))
+
+
+class ErrorAggregatorDict(defaultdict):
+    def __init__(self):
+        super(ErrorAggregatorDict, self).__init__(ErrorAggregator)
+
+    def log(self):
+        for name in sorted(list(self.keys())):
+            log.info(f"Top 10 errors in {name} ({len(self[name])} total):\n{self[name]}")
 
 
 class PyTorchModuleExtractor(object):
@@ -83,14 +103,29 @@ class PyTorchModuleExtractor(object):
     then test if they function correctly with the JIT.
     """
 
-    def __init__(self, errors):
+    def __init__(self, errors=None, stats=None):
         super().__init__()
-        self.output_module = types.ModuleType(f"{__name__}.output")
-        self.module_names = set()
-        self.module_statements = []
-        self.imports = dict()
-        self.stats = Stats()
+        if errors is None:
+            errors = ErrorAggregatorDict()
+        if stats is None:
+            stats = Stats()
+        self.stats = stats
         self.errors = errors
+
+        self.output_module = types.ModuleType(f"{__name__}.output")
+        self._tempdir = tempfile.TemporaryDirectory(prefix="paritybench")
+        self.module_names = set()
+
+        self.imports = dict()
+        self.constants = []
+        self.module_statements = []
+
+    def __enter__(self):
+        self.tempdir = self._tempdir.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._tempdir.__exit__(exc_type, exc_val, exc_tb)
 
     def add_module_alias(self, name: str):
         if "name" in self.output_module.__dict__:
@@ -122,6 +157,9 @@ class PyTorchModuleExtractor(object):
         except Exception as e:
             return self.errors["parse"].record(e)
 
+        self.search_ast(tree)
+
+    def search_ast(self, tree: ast.AST):
         for node in ast.iter_child_nodes(tree):
             if isinstance(node, ast.ClassDef):
                 bases = [to_source(x).strip() for x in node.bases]
@@ -134,6 +172,16 @@ class PyTorchModuleExtractor(object):
                 if re.match(r"(from ({0})\b)|(import ({0})\b)".format("|".join(IMPORT_WHITELIST)),
                             node_str):
                     self.imports[node_str] = node
+            elif isinstance(node, ast.Assign) and self.is_literal(node.value):
+                self.constants.append(node)
+
+    @staticmethod
+    def is_literal(node):
+        try:
+            ast.literal_eval(node)
+            return True
+        except ValueError:
+            return False
 
     def search_directory(self, filename: str):
         for root, _, files in os.walk(filename, topdown=False):
@@ -161,17 +209,32 @@ class PyTorchModuleExtractor(object):
     def construct_module(self):
         for statement in self.imports.values():
             try:
-                self.add_statement(statement)
+                self.run_statement(statement)
             except Exception as e:
                 self.errors["import"].record(e)
+        for statement in self.constants:
+            try:
+                self.run_statement(statement)
+            except Exception as e:
+                self.errors["constant"].record(e)
         for statement in self.module_statements:
             try:
-                self.add_statement(statement)
+                fixed = ast.fix_missing_locations(ASTCleanup().visit(statement))
+                self.run_statement(fixed, source_required=True)
             except Exception as e:
                 self.errors["define"].record(e)
 
-    def add_statement(self, statement):
-        code = compile(ast.Module([statement], []), "<string>", "exec")
+    def run_statement(self, statement, source_required=False):
+        if not source_required:
+            code = compile(ast.Module([statement], []), "<string>", "exec")
+        else:
+            # TorchScript requires source code to exist on disk
+            assert self.tempdir
+            fn, filename = tempfile.mkstemp(suffix='.py', dir=self.tempdir, )
+            with os.fdopen(fn, "w") as fd:
+                fd.write(to_source(statement))
+                fd.flush()
+            code = compile(open(filename).read(), filename, "exec")
         exec(code, self.output_module.__dict__, self.output_module.__dict__)
 
     @staticmethod
@@ -189,16 +252,8 @@ class PyTorchModuleExtractor(object):
         return nn_cls(**kwargs)
 
     def guess_arg_value(self, name: str, annotation=inspect.Parameter.empty):
-        common_args = {
-            "channels": 3,
-            "num_classes": 2,
-            "scale_factor": 1.0,
-        }
-        for search_name, placeholder_value in common_args.items():
-            if search_name in name:
-                return placeholder_value
         # TODO(jansel): use annotation / support guessing tensor values
-        raise NotImplementedError(f"Cant guess value for: {name}")
+        return MockConfig()[name]
 
     def test_modules(self):
         for name, value in list(self.output_module.__dict__.items()):
@@ -216,30 +271,41 @@ class PyTorchModuleExtractor(object):
             return self.errors['init'].record(e)
         self.stats["init_ok"] += 1
 
+        signature = inspect.signature(nn_module.forward)
         try:
-            signature = inspect.signature(nn_module.forward)
-            num_args = len(list(self.needed_args(signature)))
-            arg_shapes = DeduceArgShapes(nn_module, num_args).search()
-            if not arg_shapes:
-                return
+            arg_shapes = DeduceArgShapes.search_retry(nn_module, list(self.needed_args(signature)))
             args = [torch.rand(shape) for shape in arg_shapes]
+            python_output = nn_module(*args)
         except Exception as e:
             return self.errors['deduce'].record(e)
         self.stats["deduced_args_ok"] += 1
 
         try:
-            torch.jit.script(nn_module)
+            # JitTestCase().checkScript(nn_module, args)
+            script = torch.jit.script(nn_module)
+            script_output = script(*args)
+            self.assertEqual(script_output, python_output)
         except Exception as e:
             return self.errors['compile'].record(e)
-        self.stats["jit_compiles"] += 1
-
-        try:
-            JitTestCase().checkScript(nn_module, args)
-        except Exception as e:
-            return self.errors['check'].record(e)
 
         self.stats["jit_success"] += 1
         log.info(f"CORRECT: {name}")
+
+    def assertEqual(self, a, b):
+        tc = unittest.TestCase()
+        if isinstance(a, torch.Tensor):
+            tc.assertTrue(torch.allclose(a, b))
+        elif isinstance(a, (list, tuple)):
+            tc.assertEqual(len(a), len(b))
+            for a_, b_ in zip(a, b):
+                self.assertEqual(a_, b_)
+        elif isinstance(a, dict):
+            keys = set(a.keys())
+            tc.assertEqual(keys, set(b.keys()))
+            for key in keys:
+                self.assertEqual(a[key], b[key])
+        else:
+            tc.assertEqual(a, b)
 
     def debug_output(self, filename: str = "output.py"):
         with open(filename, "w") as out:
@@ -256,21 +322,70 @@ class PyTorchModuleExtractor(object):
                 out.write("\n")
 
 
+class MockConfig(object):
+    """
+    Try to get an unknown nn.Module's init to run by guessing values
+    """
+    def __getitem__(self, name):
+        common_args = {
+            "channels": 3,
+            "num_classes": 2,
+            "scale_factor": 1.0,
+            "nclass": 2,
+            "dim": DeduceArgShapes.default_size,
+            "planes": DeduceArgShapes.default_size,
+            "size": DeduceArgShapes.default_size,
+            "config": self,
+            "cfg": self,
+        }
+        for search_name, placeholder_value in common_args.items():
+            if search_name in name:
+                return placeholder_value
+        raise NotImplementedError(f"{name}")
+
+
+class ASTCleanup(ast.NodeTransformer):
+    pass
+
+
 class DeduceArgShapes(object):
     """
     Try to figure out a valid input for an NN module by repeated
     guessing based on error messages.
     """
-    _default_dim = 16
+    default_size = 8
 
-    def __init__(self, nn_module, num_args):
+    def __init__(self, nn_module, initial_guess):
         super(DeduceArgShapes, self).__init__()
         self.nn_module = nn_module
         self.name = nn_module.__class__.__name__
-        self.shape_guess = [[self._default_dim] + [n + self._default_dim] * (n + 3) for n in range(num_args)]
-        self.shape_tried = [set() for _ in range(num_args)]
+        self.shape_guess = initial_guess
+        self.shape_tried = [set() for _ in range(len(initial_guess))]
         self.attempt_log = []
         self.give_up = False
+        self.last_error = None
+
+    @classmethod
+    def search_retry(cls, nn_module, needed_args):
+        num_args = len(needed_args)
+        deducers = [cls(nn_module, initial_guess)
+                    for initial_guess in (
+                        # cls.initial_guess1(num_args),
+                        cls.initial_guess2(num_args),
+                    )]
+        for deducer in deducers:
+            shape = deducer.search()
+            if shape:
+                return shape
+        raise random.choice(deducers).last_error
+
+    @classmethod
+    def initial_guess1(cls, num_args):
+        return [[cls.default_size] * 3 for _ in range(num_args)]
+
+    @classmethod
+    def initial_guess2(cls, num_args):
+        return [[cls.default_size] + [n + cls.default_size] * (n + 3) for n in range(num_args)]
 
     def search_once(self):
         guess = list(self.shape_guess)
@@ -283,6 +398,7 @@ class DeduceArgShapes(object):
             log.info(f"{self.name}: OK ({guess})")
             return guess
         except Exception as e:
+            self.last_error = e
             msg = str(e)
             self.attempt_log.append((guess, e.__class__.__name__, msg))
 
@@ -316,11 +432,6 @@ class DeduceArgShapes(object):
                 return result
             if self.give_up:
                 break
-
-        log.info("---")
-        for attempt in self.attempt_log:
-            log.info(f"{self.name}: {attempt}")
-        log.warning(f"{self.name}: GIVING UP")
 
     def ideas_from_error(self, guessed_shape: List[int], error_msg: str) -> List[List[int]]:
         guesses = []
@@ -375,7 +486,7 @@ class DeduceArgShapes(object):
             got = int(match.group(1))
             need = int(match.group(2))
             if len(guessed_shape) == got:
-                guess = [self._default_dim] * need
+                guess = [self.default_size] * need
                 guesses.append((0, guess))
 
         match = re.search(
@@ -410,7 +521,7 @@ class DeduceArgShapes(object):
         return guesses
 
     def convolution_guess(self, weight: List[int], groups: int = 1):
-        return [self._default_dim, weight[1] * groups] + [x * self._default_dim for x in weight[2:]]
+        return [self.default_size, weight[1] * groups] + [x * self.default_size for x in weight[2:]]
 
 
 class CrawlGitHub(object):
@@ -465,16 +576,14 @@ class CrawlGitHub(object):
 
     def test_all(self):
         stats = Stats()
-        error_aggregators = defaultdict(ErrorAggregator)
+        error_aggregators = ErrorAggregatorDict()
         for filename in os.listdir(self.download_dir):
             if filename.endswith('.zip'):
-                stats_part = PyTorchModuleExtractor(error_aggregators).main(os.path.join(self.download_dir, filename))
-                stats.update(stats_part)
-        log.info(f"{len(error_aggregators)}")
-        for name, errors in sorted(error_aggregators.items()):
-            log.info(f"Top 10 errors in {name}:\n{errors}")
+                with PyTorchModuleExtractor(error_aggregators) as extractor:
+                    extractor.main(os.path.join(self.download_dir, filename))
+                    stats.update(extractor.stats)
+        error_aggregators.log()
         stats.log('TOTAL')
-
 
 
 if __name__ == "__main__":
